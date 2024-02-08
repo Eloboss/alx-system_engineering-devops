e3d3fb06b4eb2843d4db8bdcebb073396cc047c4
Eloboss <111271524+Eloboss@users.noreply.github.com>
2024-02-08 04:10:16 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,8 +0,0 @@
-# Using Puppet, create a manifest that kills a process named killmenow
-
-exec { 'killing a process':
-  command  => 'pkill -9 killmenow',
-  path     => '/usr/bin:/bin',
-  onlyif   => 'pgrep killmenow',
-  provider => shell,
-}
